d411f4ffd18b8ba5e080b4e5173698e22d63ec3e
Dustin Reynolds <reynol2@pdx.edu>
2015-10-12 23:43:11 -0700

    Add support to delete database entries

--- nodetest2/routes/users.js
+++ nodetest2/routes/users.js
@@ -20,9 +20,21 @@ router.post('/adduser', function(req,res) {
 	var collection = db.get('userlist');
 	collection.insert(req.body, function(err, result) {
 		res.send(
-			(err === null) ? { msg: ''} : { msg: err }
+			(err === null) ? { msg: '' } : { msg: err }
 		);
 	});
 });
 
 module.exports = router;
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
